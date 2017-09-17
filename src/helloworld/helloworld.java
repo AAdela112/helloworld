8f48ea560fe8033a55498fca8386a6a6d85ab72c
adela <18354216505@163.com>
2017-09-17 10:27:06 +0800

    test

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,7 +4,7 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("helloworld随便改一改就好啦");
+		System.out.println("helloworld世界你好");
 	}
 
 }
